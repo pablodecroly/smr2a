b8c9fda8d89983cbe93d97fbc9fee35b404b7e36
pablodecroly <io2xxpablo@gmail.com>
2019-10-02 16:47:41 +0200

    Catálogo

---
+++ catalogo.php
@@ -0,0 +1,30 @@
+<center>
+Pablo
+<br>
+</center>
+
+<html>
+<center>
+	<body>
+		<h1>PERFILES GITHUB</h1>
+	</body>
+	<a href="https://github.com/dblancoformacion" target="_blank">David (Profesor)</a>
+	<br>
+	<a href="https://github.com/pablodecroly" target="_blank">Pablo</a>
+	<br>
+	<a href="https://github.com/anabelladecroly" target="_blank">Anabella</a>
+	<br>
+	<a href="https://github.com/gabrieldecroly" target="_blank">Gabriel</a>
+	<br>
+	<a href="https://github.com/jacs24" target="_blank">Jose</a>
+	<br>
+	<a href="https://github.com/Alberto-cloud" target="_blank">Alberto</a>
+	<br>
+	<a href="https://github.com/VictorCaboRamirez" target="_blank">Victor C.</a>
+	<br>
+	<a href="https://github.com/victorflorea" target="_blank">Victor F.</a>
+	<br>
+	<a href="https://github.com/Jhoss11" target="_blank">Jhostin</a>
+</center>
+	
+</html>	
